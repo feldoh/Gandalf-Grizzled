bd1fb56d4b083ea87ea0b6503f7455e6f4fc98db
Dexter Lowe <feldoh10@gmail.com>
2022-05-02 16:49:28 +0100

    Simplified Teleporting Sickness

--- 1.3/Source/Grizzled_Gandalf/Hediff_RandomTeleport.cs
+++ 1.3/Source/Grizzled_Gandalf/Hediff_RandomTeleport.cs
@@ -1,5 +1,6 @@
 using System;
 using RimWorld;
+using UnityEngine;
 using Verse;
 using Verse.Sound;
 
@@ -7,44 +8,64 @@ namespace Grizzled_Gandalf
 {
 	public class Hediff_RandomTeleport : HediffWithComps
 	{
-		public override void PostAdd(DamageInfo? dinfo)
+		private int teleportInterval;
+		private int teleportRange;
+		private float teleportFactor;
+		private const int OneDayTicks = 60000;
+		private const int TeleportBaseRange = 50;
+
+		public override void PostMake()
 		{
-			base.PostAdd(dinfo);
-			UpdateTarget();
+			base.PostMake();
+			teleportFactor = Rand.Range(0.8f, 2.0f);
+			UpdateTeleportSeverity();
 		}
-		
+
+		public override void ExposeData()
+		{
+			base.ExposeData();
+			Scribe_Values.Look(ref teleportFactor, "teleportFactor");
+		}
+
+		private void UpdateTeleportSeverity()
+		{
+			teleportInterval = Mathf.Clamp((int)((1.0f - Severity) * teleportFactor) * OneDayTicks, 250,
+				(int)(OneDayTicks * teleportFactor));
+			teleportRange = Mathf.Clamp((int)(Severity * teleportFactor * TeleportBaseRange), 10, pawn.Map.Size.x);
+		}
+
+		private bool ShouldTeleport()
+		{
+			return !pawn.IsHashIntervalTick(teleportInterval);
+		}
+
 		public override void Tick()
 		{
 			base.Tick();
-			if (!this.pawn.IsHashIntervalTick(250))
-			{
-				UpdateTarget();
-			}
-		
+			if (!ShouldTeleport())
+				return;
+			UpdateTarget();
+			UpdateTeleportSeverity();
 		}
 
 		private void UpdateTarget()
 		{
-			Pawn pawn = this.pawn;
-			if (pawn.Spawned)
-			{
-				var pawnMap = pawn.Map;
-				IntVec3 approxDest = pawn.OccupiedRect().ExpandedBy(50).RandomCell.ClampInsideMap(pawnMap);
-				if (this.FindFreeCell(approxDest, pawnMap, out var result)) {
-					pawn.teleporting = true;
-					pawn.Position = result;
-					FleckMaker.ThrowDustPuffThick(result.ToVector3(), pawnMap, Rand.Range(1.5f, 3f), CompAbilityEffect_Chunkskip.DustColor);
-					pawn.teleporting = false;
-					pawn.stances.stunner.StunFor(new IntRange(50, 150).RandomInRange, (Thing)pawn, false);
-					pawn.Notify_Teleported();
-					SoundDefOf.Psycast_Skip_Pulse.PlayOneShot((SoundInfo) new TargetInfo(approxDest, pawnMap));
-					pawn.health?.RemoveHediff(this);
-				}
-			}
+			if (!pawn.Spawned) return;
+			var pawnMap = pawn.Map;
+			IntVec3 approxDest = pawn.OccupiedRect().ExpandedBy(teleportRange).RandomCell.ClampInsideMap(pawnMap);
+			if (!this.FindFreeCell(approxDest, pawnMap, out var result)) return;
+			pawn.teleporting = true;
+			pawn.Position = result;
+			FleckMaker.ThrowDustPuffThick(result.ToVector3(), pawnMap, Rand.Range(1.5f, 3f),
+				CompAbilityEffect_Chunkskip.DustColor);
+			pawn.teleporting = false;
+			pawn.stances.stunner.StunFor(new IntRange(50, 150).RandomInRange, pawn, false);
+			pawn.Notify_Teleported();
+			SoundDefOf.Psycast_Skip_Pulse.PlayOneShot(new TargetInfo(approxDest, pawnMap));
 		}
-		
-		private bool FindFreeCell(IntVec3 target, Map map, out IntVec3 result) => CellFinder.TryFindRandomSpawnCellForPawnNear(target, map, out result, 10, (Predicate<IntVec3>) (cell => CompAbilityEffect_WithDest.CanTeleportThingTo((LocalTargetInfo) cell, map)));
 
+		private bool FindFreeCell(IntVec3 target, Map map, out IntVec3 result) =>
+			CellFinder.TryFindRandomSpawnCellForPawnNear(target, map, out result, 10,
+				(Predicate<IntVec3>)(cell => CompAbilityEffect_WithDest.CanTeleportThingTo((LocalTargetInfo)cell, map)));
 	}
-	
 }
