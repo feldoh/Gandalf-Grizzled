3b2db70a104fece0db176f2e7ec7ed7e31a746d5
Dexter Lowe <feldoh10@gmail.com>
2022-05-02 19:04:01 +0100

    Teleporting Sickness Log Events

---
+++ 1.3/Source/Grizzled_Gandalf/Illness/HediffDef_RandomTeleport.cs
@@ -0,0 +1,10 @@
+using Verse;
+
+namespace Grizzled_Gandalf.Illness
+{
+	public class HediffDef_RandomTeleport: HediffDef
+	{
+		private const int OneHourTicks = 2500;
+		public int minimumTicksBetween = OneHourTicks;
+	}
+}
